bf004e3018ef46fb16837b1904f70da41f8d1d4c
Javier Picón <82212139+MCPikon@users.noreply.github.com>
2024-05-30 19:55:16 +0200

    added new project and updated dependencies

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const INSTAGRAM_URL: string = "https://instagram.com/mcpikon"
 export const GITHUB_URL: string = "https://github.com/MCPikon"
-export const TWITTER_URL: string = "https://twitter.com/mcpikon"
+export const TWITTER_URL: string = "https://x.com/mcpikon"
 export const LINKEDIN_URL: string = "https://www.linkedin.com/in/javierpiconpastor/"
 export const EMAIL: string = "javierpiconpastor@gmail.com"
