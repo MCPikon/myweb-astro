2aa271847fb93b7c65a72be1fb428a7d22f20b39
Javier Picón <82212139+MCPikon@users.noreply.github.com>
2026-03-09 19:07:47 +0100

    removed reactjs and updated some pkgs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,36 +1,39 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
-import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
 import robotsTxt from "astro-robots-txt";
-
-import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://javier-picon.vercel.app",
   prefetch: true,
-  integrations: [tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), compress(), react(), sitemap(), partytown({
-    config: {
-      forward: ["dataLayer.push"]
-    }
-  }), robotsTxt({
-    sitemap: [
-      'https://javier-picon.vercel.app/sitemap-0.xml',
-      'http://javier-picon.vercel.app/sitemap-index.xml',
-    ],
-  }), playformCompress()],
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    compress(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    robotsTxt({
+      sitemap: [
+        "https://javier-picon.vercel.app/sitemap-0.xml",
+        "http://javier-picon.vercel.app/sitemap-index.xml",
+      ],
+    }),
+  ],
   i18n: {
-    defaultLocale: 'es',
-    locales: ['es', 'en'],
+    defaultLocale: "es",
+    locales: ["es", "en"],
     routing: {
-      prefixDefaultLocale: false
-    }
-  }
+      prefixDefaultLocale: false,
+    },
+  },
 });
