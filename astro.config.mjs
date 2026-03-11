2fadce2dd5ed0e56318fbf992e9b9281d80a3cae
Javier Picón <82212139+MCPikon@users.noreply.github.com>
2026-03-11 23:37:52 +0100

    updated i18n texts

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://javier-picon.vercel.app",
-  prefetch: true,
   vite: {
     plugins: [tailwindcss()],
   },
